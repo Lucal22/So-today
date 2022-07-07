c2dc83e07c6e10a57ced6804d9c08f547324b420
Luis Carlos <lucaldev22@gmail.com>
2022-07-07 02:27:49 -0300

    Fixing responsive layout

--- src/container/Homepage/components/LatestPost.tsx
+++ src/container/Homepage/components/LatestPost.tsx
@@ -14,8 +14,8 @@ export default function LatestPost({lastPostUrl, lastPostTitle}: LatestPostProps
     className='rounded-t-[10px] cursor-pointer max-w-full'
     src={lastPostUrl} />
     </a>
-    <div className='flex justify-center align-middle  max-w-full h-14 p-2 rounded-br-[10px] bg-darkbg dark:bg-slate-200'>
-      <h1 className=' dark:text-black text-white ' >
+    <div className='flex justify-center align-middle  max-w-full h-100 p-2 rounded-br-[10px] bg-darkbg dark:bg-slate-200'>
+      <h1 className=' dark:text-black text-white text-lg md:text-2xl ' >
       <a className='hover:text-blue-500 dark:hover:text-blue-500'
       href=''>{lastPostTitle}</a>
         </h1>
